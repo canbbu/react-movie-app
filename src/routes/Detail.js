55bccb693e623e0f152d856530408b430336043b
canbbu222 <canbbu222@gmail.com>
2024-09-27 22:28:03 +0900

    detail added

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Detail.css"; // CSS 파일을 따로 만들어 스타일을 적용
 
 class Detail extends React.Component {
   componentDidMount() {
@@ -7,13 +8,28 @@ class Detail extends React.Component {
       history.push("/");
     }
   }
+
   render() {
     const { location } = this.props;
+    console.log(this.props)
     if (location.state) {
-      return <span>{location.state.title}</span>;
+      return (
+        <div className="detail__container">
+          <div className="detail__poster">
+            <img src={location.state.poster} alt={location.state.title} />
+          </div>
+          <div className="detail__info">
+            <h1>{location.state.title}</h1><br/>
+            <h2>{location.state.genres}</h2><br/>
+            <h3>{location.state.year}</h3><br/>
+            <p>{location.state.summary}</p>
+          </div>
+        </div>
+      );
     } else {
       return null;
     }
   }
 }
+
 export default Detail;
