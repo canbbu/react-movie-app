8bc254d4984bd9a9dab5f129d33990d2c0a3cddd
canbbu222 <canbbu222@gmail.com>
2024-09-29 15:26:34 +0900

    toolbar added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,94 @@
-import React from "react";
-import { HashRouter, Route } from "react-router-dom";
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate, HashRouter } from 'react-router-dom';
 import Home from "./routes/Home";
 import About from "./routes/About";
 import Detail from "./routes/Detail";
+import Layout from "./components/Layout";
 import Navigation from "./components/Navigation";
 import "./App.css";
 
 function App() {
+  const [login, setLogin] = useState(false);
+  const [username, setUsername] = useState('');
+  const [searchKeyword, setSearchKeyword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleLogout = () => {
+    localStorage.removeItem('login');
+    localStorage.removeItem('username');
+    setLogin(false);
+    setUsername('');
+    setSearchKeyword('');
+  };
+
+  const handleValueChange = (e) => {
+    setSearchKeyword(e.target.value);
+  };
+
+  const handleLogin = async (username, password) => {
+    try {
+      console.log("App.js: username:", username, "password:", password);
+      const response = await fetch('/api/items/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          username: username,
+          password: password,
+        }),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        // login success
+        localStorage.setItem('login', 'true');
+        localStorage.setItem('username', username); // user name store
+        setLogin(true);
+        setUsername(username);
+        setErrorMessage('');
+
+        // data fetch after login
+        const itemsResponse = await fetch('/api/items', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+
+        if (itemsResponse.ok) {
+          const itemsData = await itemsResponse.json();
+          //setItems(itemsData);
+        } else {
+          console.error('Failed to fetch items data');
+        }
+      } else {
+        // login fail
+        setErrorMessage(data.message || 'Login failed');
+      }
+    } catch (error) {
+      console.error('Error during login:', error);
+      setErrorMessage('An error occurred');
+    }
+  };
+
   return (
     <HashRouter>
+      <Layout
+        login={login}
+        handleLogout={handleLogout}
+        searchKeyword={searchKeyword}
+        handleValueChange={handleValueChange}
+        handleLogin={handleLogin}
+        username={username}
+      />
       <Navigation />
-      <Route path="/" exact={true} component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/movie/:id" component={Detail} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/movie/:id" element={<Detail />} />
+      </Routes>
     </HashRouter>
   );
 }
---
+++ src/components/Layout.js
@@ -0,0 +1,159 @@
+import React, { useState } from 'react';
+import { Outlet, useNavigate, Link } from 'react-router-dom';
+import { AppBar, Toolbar, Typography, IconButton, Button, Box, Menu, MenuItem } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import { styled, alpha } from '@mui/material/styles';
+import InputBase from '@mui/material/InputBase';
+import SearchIcon from '@mui/icons-material/Search';
+import { ToggleButton, ToggleButtonGroup } from '@mui/material';
+
+// Styled components
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(1),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  width: '100%',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
+}));
+
+function Layout({ login, handleLogout, searchKeyword, handleValueChange, handleLogin, username }) {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [alignment, setAlignment] = useState('ALL');
+  const navigate = useNavigate();
+
+  const handleMenuClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleToggleChange = (event, newAlignment) => {
+    setAlignment(newAlignment);
+    handleMenuClose();
+
+    // Navigate to the respective pages based on the selection
+    switch (newAlignment) {
+      case 'ALL':
+        navigate('/all');
+        break;
+      case 'KOREA':
+        navigate('/korea');
+        break;
+      case 'JAPAN':
+        navigate('/japan');
+        break;
+      default:
+        break;
+    }
+  };
+
+  return (
+    <div>
+      {/* Toolbar */}
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }} onClick={handleMenuClick}>
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              anchorEl={anchorEl}
+              open={Boolean(anchorEl)}
+              onClose={handleMenuClose}
+              keepMounted
+            >
+              {login ? (
+                <MenuItem>
+                  {/* Toggle Button Group in the Menu */}
+                  <ToggleButtonGroup
+                    value={alignment}
+                    exclusive
+                    onChange={handleToggleChange}
+                    aria-label="text alignment"
+                  >
+                    <ToggleButton value="ALL" aria-label="ALL">
+                      ALL
+                    </ToggleButton>
+                    <ToggleButton value="KOREA" aria-label="KOREA">
+                      KOREA
+                    </ToggleButton>
+                    <ToggleButton value="JAPAN" aria-label="JAPAN">
+                      JAPAN
+                    </ToggleButton>
+                  </ToggleButtonGroup>
+                </MenuItem>
+              ) : null}
+            </Menu>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              <Link to="/" style={{ textDecoration: 'none', color: 'inherit' }}>
+                GudeHolic System
+              </Link>
+            </Typography>
+            {login ? (
+              <React.Fragment>
+                {/* Login state toolbar and toggle button  */}
+                <Search>
+                  <SearchIconWrapper>
+                    <SearchIcon />
+                  </SearchIconWrapper>
+                  <StyledInputBase
+                    inputProps={{ 'aria-label': 'search' }}
+                    name="searchKeyword"
+                    value={searchKeyword}
+                    onChange={handleValueChange}
+                  />
+                </Search>
+                <Button variant="contained" color="primary" onClick={handleLogout}>
+                  Logout
+                </Button>
+              </React.Fragment>
+            ) : (
+              // If not logged in, you might want to render a login component here
+              <Button variant="contained" color="primary" onClick={() => handleLogin(username, 'password')}>Login</Button>
+            )}
+          </Toolbar>
+        </AppBar>
+      </Box>
+
+      {/* Render nested routes here */}
+      <Outlet />
+    </div>
+  );
+}
+
+export default Layout;
