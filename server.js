1b827fdd013437e53f57ccbf32d368abb8dd670c
canbbu222 <canbbu222@gmail.com>
2024-10-01 22:21:08 +0900

    loginBackend

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = process.env.PORT || 5000;
+const ctrl = require('./server.ctrl');
+const router = express.Router();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// 이미지 파일 제공
+app.use('/image', express.static('upload'));
+
+// 라우터를 app에 연결
+app.use('/', router);
+
+// multer 설정
+const multer = require('multer');
+const upload = multer({ dest: './upload' });
+
+// 라우터 설정
+// router.get('/', ctrl.output.dataDisplay);
+// router.post('/', upload.single('image'), ctrl.process.dataUpdate);
+router.post('/login', ctrl.process.login);
+// router.delete('/:id', ctrl.process.delete);
+
+// 서버 시작
+app.listen(port, () => console.log(`Listening on port ${port}`));
